f17b9c82cc0eb86678319123c7d6aeb04d88b5cb
Livingston-k <kaddulivington@gmail.com>
2022-07-29 17:51:00 +0300

    qr

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
-const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const qrcode = require('qrcode')
 const PORT = process.env.PORT || 3000
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'view'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 app.get('/', (req, res) => {
-    res.send("QR CODE")
+    res.render('index');
 })
+
+app.post('/scan', (req, res) => {
+    var inputdata = req.body.qrtext;
+    qrcode.toDataURL(inputdata, (err, src) => {
+        if (err) throw err
+        res.render('scan', { qrcode: src });
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
